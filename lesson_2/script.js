f0df82aa6ecaafcf4b38c086adcc23a18eb43b60
PoniaPolla <gusakovskayadev@gmail.com>
2024-12-22 14:32:31 +0300

    ASTONJS-44 [fix]: getLength for Objects & Numbers & null&undefined & Date

--- lesson_2/script.js
+++ lesson_2/script.js
@@ -6,8 +6,21 @@ function getLength(value){
 	if(value?.length){
 		console.log(value.length);
 	}
+	else if(value === null || value === undefined){
+		console.log(0);
+	}
 	else if(value?.size){
 		console.log(value.size);
+	}
+	else if(typeof value === 'number'){
+		console.log(String(value).length);
+	}
+	else if(typeof value === 'object'){
+		if(String(value) === '[object Object]'){
+			console.log(Object.entries(value).length);
+		}else{
+			console.log(String(value).length);
+		}
 	}
 	else{
 		console.log(0);
@@ -39,54 +52,54 @@ getLength(mySet);
 
 // Напишите функцию compare, принимающую на вход два объекта Node, и возвращающую true, если они имеют одинаковую структуру и значения и false в ином случае.
 
-const tree1 = {
-  value: 1,
-  left: { value: 2, left: undefined, right: undefined },
-  right: { value: 3, left: undefined, right: undefined },
-};
+// const tree1 = {
+//   value: 1,
+//   left: { value: 2, left: undefined, right: undefined },
+//   right: { value: 3, left: undefined, right: undefined },
+// };
 
-const tree2 = {
-  value: 1,
-  left: { value: 2, left: undefined, right: undefined },
-  right: { value: 3, left: undefined, right: undefined },
-};
+// const tree2 = {
+//   value: 1,
+//   left: { value: 2, left: undefined, right: undefined },
+//   right: { value: 3, left: undefined, right: undefined },
+// };
 
-const tree3 = {
-  value: 1,
-  left: { value: 3, left: undefined, right: undefined },
-  right: { value: 3, left: undefined, right: undefined },
-};
+// const tree3 = {
+//   value: 1,
+//   left: { value: 3, left: undefined, right: undefined },
+//   right: { value: 3, left: undefined, right: undefined },
+// };
 
-const tree4 = {
-  value: 1,
-  left: { 
-    value: 2, 
-    left: { 
-      value: 3, 
-      left: null, 
-      right: null 
-    },
-    right: null 
-  },
-  right: null
-};
+// const tree4 = {
+//   value: 1,
+//   left: { 
+//     value: 2, 
+//     left: { 
+//       value: 3, 
+//       left: null, 
+//       right: null 
+//     },
+//     right: null 
+//   },
+//   right: null
+// };
 
-function compare(node1, node2) {
-  const stack = [{ n1: node1, n2: node2 }];
+// function compare(node1, node2) {
+//   const stack = [{ n1: node1, n2: node2 }];
 
-  while (stack.length > 0) {
-    const { n1, n2 } = stack.pop();
+//   while (stack.length > 0) {
+//     const { n1, n2 } = stack.pop();
 
-    if (n1 === undefined && n2 === undefined) continue;
-    if (n1 === undefined || n2 === undefined || n1.value !== n2.value) return false;
+//     if (n1 === undefined && n2 === undefined) continue;
+//     if (n1 === undefined || n2 === undefined || n1.value !== n2.value) return false;
 
-    stack.push({ n1: n1.right, n2: n2.right });
-    stack.push({ n1: n1.left, n2: n2.left });
-  }
+//     stack.push({ n1: n1.right, n2: n2.right });
+//     stack.push({ n1: n1.left, n2: n2.left });
+//   }
 
-  return true;
-}
+//   return true;
+// }
 
-console.log(compare(tree1, tree2));
-console.log(compare(tree1, tree3));
-console.log(compare(tree1, tree4));
+// console.log(compare(tree1, tree2));
+// console.log(compare(tree1, tree3));
+// console.log(compare(tree1, tree4));
